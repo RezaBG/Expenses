c6c419b464b53d352aeaf4b982244a4fd0755554
rezaBG <rezabarzegargashti@gmail.com>
2023-04-16 19:36:06 +0200

    init files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Product from "./components/Product/Product";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div id="main" className="container">
+        <h2>React App</h2>
+        <Product title="Book1" price="99" />
+        <Product title="Book2" price="89" />
+        <Product title="Book3" price="109" />
+      </div>
+    );
+  }
 }
 
 export default App;
