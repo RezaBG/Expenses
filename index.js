35f520de39f08751f5e974022c72304ec496c183
rezaBG <rezabarzegargashti@gmail.com>
2023-04-24 18:38:22 +0200

    init 2

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const page = (
-  <div>
-    <img src="./react-logo.png" width="40px" />
-    <h1>FUnc fact about React</h1>
-    <ul>
-      <li>fact 1</li>
-      <li>fact 2</li>
-      <li>fact 3</li>
-      <li>fact 4</li>
-    </ul>
-  </div>
-);
+import App from "./App";
 
-ReactDOM.render(page, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
