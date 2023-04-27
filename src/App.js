0b2fefdd905ac91e9ca4a1f7f89ec548c503dc91
Reza Barzergar Gashti <101470327+RezaBG@users.noreply.github.com>
2023-04-27 12:04:05 +0000

    add date to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expense = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2023, 3, 5),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2023, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2023, 5, 12),
+    },
+  ];
+
   return (
     <div>
       <h1>Let's Start</h1>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}
+      />
+      <ExpenseItem
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={expense[1].date}
+      />
+      <ExpenseItem
+        title={expense[2].title}
+        amount={expense[2].amount}
+        date={expense[2].date}
+      />
+      <ExpenseItem
+        title={expense[3].title}
+        amount={expense[3].amount}
+        date={expense[3].date}
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,10 @@
 import "./ExpenseItem.css";
 
 export default function ExpenseItem() {
+  const exppenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurence";
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
       <div>March 28th 2023</div>
