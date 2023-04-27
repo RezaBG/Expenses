8738288af04de69e4efaee48d02ed0438ae246fd
Reza Barzergar Gashti <101470327+RezaBG@users.noreply.github.com>
2023-04-27 16:07:54 +0000

    Add ExpenseDate

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,3 @@
-// import "./ExpenseItem.css";
-
-// export default function ExpenseItem(props) {
-//   const expenseDate = new Date(2021, 2, 28);
-//   const expenseTitle = "Car Insurence";
-//   const expenseAmount = 294.67;
-
-//   return (
-//     <div className="expense-item">
-//       <div>March 28th 2023</div>
-//       <div className="expense-item__description">
-//         <h2>Car Insurance</h2>
-//         <div className="expense-item__price">$289.23</div>
-//       </div>
-//     </div>
-//   );
-// }
-
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
