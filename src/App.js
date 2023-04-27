38a7ca601349a2d28539ae413ba6fae87345ce06
Reza Barzergar Gashti <101470327+RezaBG@users.noreply.github.com>
2023-04-27 12:42:52 +0000

    update app

--- src/App.js
+++ src/App.js
@@ -1,50 +1,51 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  const expense = [
+  const expenses = [
     {
       id: "e1",
       title: "Toilet Paper",
       amount: 94.12,
-      date: new Date(2023, 3, 5),
+      date: new Date(2020, 7, 14),
     },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
-      date: new Date(2023, 2, 28),
+      date: new Date(2021, 2, 28),
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
-      date: new Date(2023, 5, 12),
+      date: new Date(2021, 5, 12),
     },
   ];
 
   return (
     <div>
-      <h1>Let's Start</h1>
+      <h2>Let's get started!</h2>
       <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      />
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
       <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      />
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
       <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      />
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
       <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      />
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,41 @@
+// import "./ExpenseItem.css";
+
+// export default function ExpenseItem(props) {
+//   const expenseDate = new Date(2021, 2, 28);
+//   const expenseTitle = "Car Insurence";
+//   const expenseAmount = 294.67;
+
+//   return (
+//     <div className="expense-item">
+//       <div>March 28th 2023</div>
+//       <div className="expense-item__description">
+//         <h2>Car Insurance</h2>
+//         <div className="expense-item__price">$289.23</div>
+//       </div>
+//     </div>
+//   );
+// }
+
 import "./ExpenseItem.css";
 
-export default function ExpenseItem() {
-  const exppenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurence";
-  const expenseAmount = 294.67;
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>March 28th 2023</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$289.23</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
 }
+
+export default ExpenseItem;
