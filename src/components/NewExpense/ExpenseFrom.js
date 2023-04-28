1411374fd13767e943ec28db8c48d66587953982
Reza Barzergar Gashti <101470327+RezaBG@users.noreply.github.com>
2023-04-28 16:16:30 +0000

    update

--- src/components/NewExpense/ExpenseFrom.js
+++ src/components/NewExpense/ExpenseFrom.js
@@ -7,16 +7,37 @@ const ExpenseFrom = (props) => {
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
+
   const titleChangeHandler = (event) => {
     setEnteredTtile(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
   };
 
   const amoutChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
+    // // setUserInput({
+    // //   ...userInput,
+    // //   enteredAmount: event.target.value,
+    // // });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
   };
 
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
   };
 
   return (
