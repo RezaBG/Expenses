7a98d799a04f42ffd3f5034ea3e78e18317e0a7d
Reza Barzergar Gashti <101470327+RezaBG@users.noreply.github.com>
2023-04-28 15:19:20 +0000

    update useStates

--- src/components/NewExpense/ExpenseFrom.js
+++ src/components/NewExpense/ExpenseFrom.js
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseFrom.css";
 
 const ExpenseFrom = (props) => {
+  const [enteredTitle, setEnteredTtile] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTtile(event.target.value);
+  };
+
+  const amoutChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
   return (
     <from>
       <div className="new-expense__constols">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amoutChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2022-01-01" max="2050-01-01" />
+          <input
+            type="date"
+            min="2022-01-01"
+            max="2050-01-01"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
